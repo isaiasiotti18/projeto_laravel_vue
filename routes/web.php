5acf56ddbc00ef7d7296d849f25f5fbb42d3024a
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2023-11-23 18:13:05 -0300

    php and laravel and vue

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobrenosController;
 use App\Http\Controllers\ContatoController;
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\FornecedoresController;
+use App\Http\Controllers\ProdutosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,15 +21,19 @@ use App\Http\Controllers\ContatoController;
 |
 */
 
-Route::get('/principal', [PrincipalController::class, 'principal']);
-
-Route::get('/sobre-nos', [SobrenosController::class, 'sobrenos']);
-
-Route::get('/contato', [ContatoController::class, 'contato']);
-
-Route::get('/contato/{nome}/{email}/{telefone?}', function (
-    string $nome = '', 
-    string $email = '', 
-    string $telefone = '') {
-    echo "Estamos aqui ${nome} | email: ${email} | fone: ${telefone}";
+Route::fallback(function() {
+  echo 'Not found. <a href="/">Clique aqui</a> para retornar a página principal.';
 });
+
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobrenosController::class, 'sobrenos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', [LoginController::class, 'login'])->name('site.login');
+
+
+Route::prefix('/app')->group(function () {
+  Route::get('/fornecedores', [FornecedoresController::class, 'index'])->name('app.fornecedores');
+  Route::get('/produtos', [ProdutosController::class, 'index'])->name('app.produtos');
+  Route::get('/clientes', [ClientesController::class, 'index'])->name('app.clientes');
+});
+
