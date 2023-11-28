f9fe58cfeae8aa403dcf1e977597473a1271b30b
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2023-11-27 21:48:16 -0300

    Migrations

--- database/migrations/2023_11_27_224342_create_produtos_table.php
+++ database/migrations/2023_11_27_224342_create_produtos_table.php
@@ -11,16 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome', 100);
-            $table->text('descricao')->nullable();
-            $table->integer('peso')->default(0);
-            $table->float('preco_venda', 8, 2)->default(0);
-            $table->integer('estoque_minimo')->default(0);
-            $table->integer('estoque_maximo');
-            $table->timestamps();
-        });
+      Schema::create('produtos', function (Blueprint $table) {
+          $table->id();
+          $table->string('nome', 100);
+          $table->text('descricao')->nullable();
+          $table->integer('peso')->default(0);
+          $table->float('preco_venda', 8, 2)->default(0);
+          $table->integer('estoque_minimo')->default(0);
+          $table->integer('estoque_maximo');
+          $table->timestamps();
+      });
     }
 
     /**
--- database/migrations/2023_11_27_234839_ajuste_produtos_filiais.php
+++ database/migrations/2023_11_27_234839_ajuste_produtos_filiais.php
@@ -11,7 +11,27 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+      Schema::create("filiais", function (Blueprint $table) {
+        $table->id();
+        $table->string("filial", 30);
+        $table->timestamps();
+      });
+
+      Schema::create("produto_filiais", function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('filial_id');
+        $table->unsignedBigInteger('produto_id');
+        $table->float('preco_venda', 8, 2)->default(0);
+        $table->integer('estoque_minimo')->default(0);
+        $table->integer('estoque_maximo');
+
+        $table->foreign('filial_id')->references('id')->on('filiais');
+        $table->foreign('produto_id')->references('id')->on('produtos');
+      });
+
+      Schema::table("produtos", function (Blueprint $table) {
+        $table->dropColumn(['preco_venda', 'estoque_minimo', 'estoque_maximo']);
+      });
     }
 
     /**
