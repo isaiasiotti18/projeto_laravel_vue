055f51f2b4582f9f700407a3dea613fc5767ccb9
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2023-11-27 22:01:57 -0300

    Eloquent, Tinker

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $table = 'fornecedores';
+  protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
